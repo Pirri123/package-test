68a7def712f952cf896208ceb9fe667f3d3dd46b
Ernesto Rmz <rs.ernesto1@gmail.com>
2020-03-02 11:56:43 -0600

    finish setup

--- setup.py
+++ setup.py
@@ -8,22 +8,18 @@ setup(
   description = 'test for uploading packages to pypi',  
   author = 'Ernesto Ramirez-Sayago',                  
   author_email = 'rs.ernesto1@gmail.com',     
-  url = 'https://github.com/Pirri123/package-test/edit/master/README.md',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  url = 'https://github.com/Pirri123/package-test/edit/master/README.md',  
+  download_url = 'https://github.com/Pirri123/package-test/archive/v0.1.tar.gz', 
+  keywords = ['test'],   
+  install_requires=[ ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
 )
-view rawsetup.py hosted with ❤ by GitHub
